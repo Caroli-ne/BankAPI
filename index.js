4b21b5cdbf3d6b7768a00b0b521d5d41473a73a7
Caroli-ne <karoline_gnn@hotmail.com>
2024-05-26 12:57:24 -0300

    fix: :bug: fix ipkiss errors
    fixing errors in ipkiss tester requirements

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require ('express');
+const express = require('express');
 
 const app = express();
 
@@ -6,93 +6,76 @@ app.use(express.json());
 
 let accounts = []
 
-app.post('/reset', (req,res)=> {
+app.post('/reset', (req, res) => {
     accounts = []
     res.status(200).send('OK')
 })
 
-app.get('/balance/:account_id', (req,res)=> {
-    const {account_id} = req.params
-    const foundAccount = accounts.find((account) => account.id === parseInt(account_id))
+app.get('/balance', (req, res) => {
+    const { account_id } = req.query
+    const foundAccount = accounts.find((account) => account.id === account_id)
     if (foundAccount) {
         res.status(200).send(foundAccount.balance.toString())
-        
+    } else {
+        res.status(404).send('0')
     }
-    res.status(404).send('0')    
 })
 
-app.post('/event', (req,res)=> {
-    const {type, destination, origin, amount} = req.body;
-    let foundAccount
+app.post('/event', (req, res) => {
+    const { type, destination, origin, amount } = req.body;
+    let foundAccount;
     switch (type) {
         case 'deposit':
             foundAccount = accounts.find((account) => account.id === destination)
-            if (foundAccount){
+            if (foundAccount) {
                 foundAccount.balance += amount
-                res.status(201).send(foundAccount)
-            }
-            if (!foundAccount) {
+                res.status(201).send({ destination: foundAccount })
+            } else {
                 const newAccount = {
                     id: destination,
                     balance: amount
                 }
                 accounts.push(newAccount)
-                res.status(201).send(newAccount)
+                res.status(201).send({ destination: newAccount })
             }
-            
             break;
-    
+
         case 'withdraw':
             foundAccount = accounts.find((account) => account.id === origin)
-            if (foundAccount){
+            if (foundAccount) {
                 foundAccount.balance -= amount
-                res.status(201).send(foundAccount)
-            }
-            if (!foundAccount) {
+                res.status(201).send({ origin: foundAccount })
+            } else {
                 res.status(404).send('0')
             }
-                
             break;
+
         case 'transfer':
             const originAccount = accounts.find((account) => account.id === origin)
-            const destinationAccount = accounts.find((account) => account.id === destination)
-            if (!originAccount || !destinationAccount) {
+            let destinationAccount = accounts.find((account) => account.id === destination)
+            if (!originAccount) {
                 res.status(404).send('0')
-                break;
+            } else {
+                if (!destinationAccount) {
+                    destinationAccount = { id: destination, balance: 0 }
+                    accounts.push(destinationAccount)
+                }
+                originAccount.balance -= amount
+                destinationAccount.balance += amount
+                res.status(201).send({
+                    "origin": originAccount,
+                    "destination": destinationAccount
+                })
             }
-            originAccount.balance -= amount
-            destinationAccount.balance += amount
-            res.status(201).send({
-                "origin": originAccount,
-                "destination": destinationAccount
-            })
             break;
+
         default:
+            res.status(400).send('Unknown event type')
             break;
     }
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,() => {
-    console.log (`Servidor rodando na porta ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
 })
